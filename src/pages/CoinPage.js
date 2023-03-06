7c44cd5f24d0141c163eb2b1c83d60aa6f3bb904
jayeshuk <jayeshukalkar@gmail.com>
2023-03-06 11:21:37 +0530

    feat: loading feedback

--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -6,6 +6,7 @@ import axios from "axios";
 function CoinPage() {
   const [coin, setCoin] = useState({});
   let { coinUUID } = useParams();
+  const [loading, setLoading] = useState();
 
   const options = {
     method: "GET",
@@ -21,6 +22,7 @@ function CoinPage() {
       .then(function (response) {
         console.log("COIN", response.data);
         setCoin(response.data.data.coin);
+        setLoading(false);
       })
       .catch(function (error) {
         console.error(error);
@@ -33,62 +35,68 @@ function CoinPage() {
 
   return (
     <div className="w-[900px] pl-10">
-      <div className="flex row items-start mx-5 mt-20">
-        <div>
-          <img src={coin.iconUrl} alt={coin.name} width={40} />
-        </div>
-        <div className="ml-5">
-          <p className="font-medium">{coin.name}</p>
-          <p className="text-[#959DA5] mt-2">{coin.symbol}</p>
-          <div className="mt-4">
-            {parseFloat(coin?.change) > 0 ? (
-              <h4 className="text-xl text-[#8BC34A] flex row items-center gap-2">
-                {parseFloat(coin.change).toFixed(2)} <BsFillCaretUpFill />
-              </h4>
-            ) : (
-              <h4 className="text-xl text-[#DB4949] flex row items-center gap-2">
-                {parseFloat(coin.change).toFixed(2)} <BsFillCaretDownFill />{" "}
-              </h4>
-            )}
+      {Object.keys(coin).length ? (
+        <>
+          <div className="flex row items-start mx-5 mt-20">
+            <div>
+              <img src={coin.iconUrl} alt={coin.name} width={40} />
+            </div>
+            <div className="ml-5">
+              <p className="font-medium">{coin.name}</p>
+              <p className="text-[#959DA5] mt-2">{coin.symbol}</p>
+              <div className="mt-4">
+                {parseFloat(coin?.change) > 0 ? (
+                  <h4 className="text-xl text-[#8BC34A] flex row items-center gap-2">
+                    {parseFloat(coin.change).toFixed(2)} <BsFillCaretUpFill />
+                  </h4>
+                ) : (
+                  <h4 className="text-xl text-[#DB4949] flex row items-center gap-2">
+                    {parseFloat(coin.change).toFixed(2)} <BsFillCaretDownFill />{" "}
+                  </h4>
+                )}
 
-            <h6 className="text-sm">CHANGE</h6>
-            <hr className="my-2" />
+                <h6 className="text-sm">CHANGE</h6>
+                <hr className="my-2" />
 
-            <div className="flex row">
-              <span className="pr-4">
-                <span className="font-medium">4324.75</span>
-                <br />
-                <span className="font-light text-[#6A737D]">Buy</span>
-              </span>
-              <span className="border-l-1 pl-4  border-l-[#E1E4E8]">
-                <span className="font-medium">4668.93</span> <br />
-                <span className="font-light text-[#6A737D]">Sell</span>
-              </span>
+                <div className="flex row">
+                  <span className="pr-4">
+                    <span className="font-medium">4324.75</span>
+                    <br />
+                    <span className="font-light text-[#6A737D]">Buy</span>
+                  </span>
+                  <span className="border-l-1 pl-4  border-l-[#E1E4E8]">
+                    <span className="font-medium">4668.93</span> <br />
+                    <span className="font-light text-[#6A737D]">Sell</span>
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
 
-      <div className="m-10 ml-20">
-        <p className="mt-5">
-          <span className="font-medium text-xl">Market Cap:</span>{" "}
-          <span className="font-light text-lg text-[#252525]">{coin.marketCap}</span>
-        </p>
-        <p className="mt-5">
-          <span className="font-medium text-xl">24-Hour Volume:</span>{" "}
-          <span className="font-light text-lg text-[#252525]">{coin["24hVolume"]}</span>
-        </p>
-        <p className="mt-5">
-          <span className="font-medium text-xl">All Time High:</span>{" "}
-          <span className="font-light text-lg text-[#252525]">{coin?.allTimeHigh?.price}</span>
-        </p>
-        <p className="mt-5">
-          <span className="font-medium text-xl">Description:</span>{" "}
-          <span className="font-light text-lg text-[#252525] text-justify">
-            {coin?.description}
-          </span>
-        </p>
-      </div>
+          <div className="m-10 ml-20">
+            <p className="mt-5">
+              <span className="font-medium text-xl">Market Cap:</span>{" "}
+              <span className="font-light text-lg text-[#252525]">{coin.marketCap}</span>
+            </p>
+            <p className="mt-5">
+              <span className="font-medium text-xl">24-Hour Volume:</span>{" "}
+              <span className="font-light text-lg text-[#252525]">{coin["24hVolume"]}</span>
+            </p>
+            <p className="mt-5">
+              <span className="font-medium text-xl">All Time High:</span>{" "}
+              <span className="font-light text-lg text-[#252525]">{coin?.allTimeHigh?.price}</span>
+            </p>
+            <p className="mt-5">
+              <span className="font-medium text-xl">Description:</span>{" "}
+              <span className="font-light text-lg text-[#252525] text-justify">
+                {coin?.description}
+              </span>
+            </p>
+          </div>
+        </>
+      ) : (
+        <div className="mx-5 mt-20">Getting Coin Details...</div>
+      )}
     </div>
   );
 }
